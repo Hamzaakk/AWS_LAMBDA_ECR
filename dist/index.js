4e818229b904dff9ac3e6d0496629a4276f51442
Hamzaakk <akkaouih17@gmail.com>
2024-12-04 02:46:15 +0100

    project

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ const handler = async (event) => {
     return {
         statusCode: 200,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Hello, World!',
+        body: 'Hello, World tesr!',
     };
 };
 exports.handler = handler;
